fa42e4dcb9d9308982414389b47f06ab19271393
Wuvist <wuvist@gmail.com>
2023-06-16 15:42:58 +0800

    refine ui

--- main.go
+++ main.go
@@ -1,66 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"embed"
 	"net/http"
 
 	"github.com/go-vgo/robotgo"
 )
 
-var page = `
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<style>
-.block {
-  display: block;
-  width: 90%;
-  height: 500px;
-  border: none;
-  background-color: #04AA6D;
-  color: white;
-  padding: 14px 28px;
-  font-size: 16px;
-  cursor: pointer;
-  text-align: center;
-}
-
-.block:hover {
-  background-color: #ddd;
-  color: black;
-}
-</style>
-</head>
-<body>
-
-<button class="block" onclick="fetch('/up')">Vol Up</button>
-<br />
-<button class="block" onclick="fetch('/down')">Vol Dn</button>
-
-</body>
-</html>`
-
-func index(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, page)
-}
-
-func up(w http.ResponseWriter, req *http.Request) {
-	robotgo.Move(157, 600)
-	robotgo.Scroll(0, 10)
-}
-
-func down(w http.ResponseWriter, req *http.Request) {
-	robotgo.Move(157, 600)
-	robotgo.Scroll(0, -10)
-}
+//go:embed static/*
+var f embed.FS
+var index, _ = f.ReadFile("static/index.html")
+var genelec, _ = f.ReadFile("static/genelec.png")
 
 func main() {
-
 	robotgo.MouseSleep = 100
-	http.HandleFunc("/", index)
-	http.HandleFunc("/up", up)
-	http.HandleFunc("/down", down)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		robotgo.Move(21, 357)
+		robotgo.Click()
+		w.Write(index)
+	})
+	http.HandleFunc("/genelec.png", func(w http.ResponseWriter, req *http.Request) {
+		w.Write(genelec)
+	})
+	http.HandleFunc("/upup", func(w http.ResponseWriter, req *http.Request) {
+		for i := 1; i <= 5; i++ {
+			robotgo.Scroll(0, 10)
+		}
+	})
+
+	http.HandleFunc("/up", func(w http.ResponseWriter, req *http.Request) {
+		robotgo.Move(157, 600)
+		robotgo.Scroll(0, 10)
+	})
+	http.HandleFunc("/down", func(w http.ResponseWriter, req *http.Request) {
+		robotgo.Move(157, 600)
+		robotgo.Scroll(0, -10)
+	})
+	http.HandleFunc("/downdown", func(w http.ResponseWriter, req *http.Request) {
+		robotgo.Move(157, 600)
+		for i := 1; i <= 5; i++ {
+			robotgo.Scroll(0, -10)
+		}
+	})
 
 	http.ListenAndServe(":8888", nil)
 }
